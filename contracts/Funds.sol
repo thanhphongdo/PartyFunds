831068cf80f1442095fcda304b91c250d2ea45be
Do Thanh Phong <phongdt@mti-tech.vn>
2019-10-31 13:49:25 +0700

    update funds

--- contracts/Funds.sol
+++ contracts/Funds.sol
@@ -1,25 +1,29 @@
 pragma solidity >=0.4.22 <0.6.0;
+pragma experimental ABIEncoderV2;
 contract Funds {
     address owner;
-    uint256 totalFunds = 3320;
+    uint256 totalFunds = 0;
     uint countParty = 0;
     struct Member{
         address id;
         string name;
         uint256 money;
     }
+    
     struct Party{
         address creator;
-        address[] members;
+        address [] members;
         uint256 money;
         string message;
         string date;
         bool paySuccess;
     }
+    
     mapping(address=>Member) members;
     Member fundHost;
     Member[] memberList;
     Party[] partyList;
+    
     modifier onlyOwner() { // Modifier
         require(
             msg.sender == owner,
@@ -27,6 +31,7 @@ contract Funds {
         );
         _;
     }
+    
     modifier onlyMember() { // Modifier
         require(
             msg.sender != owner,
@@ -34,6 +39,7 @@ contract Funds {
         );
         _;
     }
+    
     modifier memberExists(address member) { // Modifier
         bool checkExists = false;
         for(uint i = 0; i < memberList.length; i++) {
@@ -48,34 +54,68 @@ contract Funds {
         );
         _;
     }
+    
+    modifier memberNotExists(address member) { // Modifier
+        bool checkExists = false;
+        for(uint i = 0; i < memberList.length; i++) {
+            if (memberList[i].id == member) {
+                checkExists = true;
+                break;
+            }
+        }
+        require(
+            !checkExists,
+            "Member is exists"
+        );
+        _;
+    }
+ 
+    
     constructor() public{
         owner = msg.sender;
         fundHost = Member(msg.sender, "HOST", 0);
     }
-    function newMember(string memory _name)public{
+    
+    function getFundHost() public view returns (Member memory) {
+        return fundHost;
+    }
+    
+    function getMemberList() public view returns (Member[] memory) {
+        return memberList;
+    }
+    
+    function getPartyList() public view returns (Party[] memory) {
+        return partyList;
+    }
+    
+    function newMember(string memory _name) public memberNotExists(msg.sender){
         Member memory member = Member(msg.sender, _name, 0);
         members[msg.sender] = member;
         memberList.push(member);
     }
+    
     function payIn(address _member, uint256 _money) public onlyOwner memberExists(_member) {
         members[_member].money += _money;
         fundHost.money += _money;
     }
+    
     function transferMoney(address _member, uint256 _money) public onlyOwner memberExists(_member) {
         members[msg.sender].money -= _money;
         members[_member].money += _money;
     }
+    
     function createParty(address[] memory _members, uint256  _money, string memory  _message, string memory  _date) public{
         partyList.push(Party(msg.sender, _members, _money, _message, _date, false));
         countParty++;
     }
-    function payForParty(uint _index) public onlyOwner{
-        if(_index >= countParty) return;
-        Party memory party = partyList[_index];
+    
+    function payForParty(uint index) public onlyOwner{
+        if(index >= countParty) return;
+        Party memory party = partyList[index];
         if(!party.paySuccess){
             party.paySuccess = true;
             uint _money = party.money / party.members.length;
-            for(uint i = 0; i <= party.members.length; i++){
+            for(uint i = 0; i< party.members.length; i++){
                 members[party.members[i]].money -= _money;
             }
             fundHost.money -= party.money;
