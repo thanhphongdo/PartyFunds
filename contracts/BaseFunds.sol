25a8f9f84945a0a52a04c19ac34cea21222c2da8
Phong Do <phongdo@innoria.com>
2019-11-11 23:34:26 +0700

    update contract

---
+++ contracts/BaseFunds.sol
@@ -0,0 +1,51 @@
+pragma solidity >=0.4.22 <0.6.0;
+pragma experimental ABIEncoderV2;
+contract BaseFunds {
+    address owner;
+    uint256 totalFunds = 0;
+    uint countMember = 0;
+    uint countParty = 0;
+    struct Member{
+        address id;
+        string name;
+        uint256 money;
+        bool isValue;
+    }
+    struct Party{
+        address creator;
+        address[] members;
+        uint256 money;
+        string message;
+        string date;
+        bool paySuccess;
+        bool isValue;
+    }
+    mapping(address=>Member) members;
+    mapping(uint=>Party) parties;
+    Member fundHost;
+    Member[] memberList;
+    Party[] partyList;
+
+    function getTotalFund() public view returns (uint256) {
+        return totalFunds;
+    }
+    function getFundHost() public view returns (Member memory) {
+        return fundHost;
+    }
+    function getMemberList() public view returns (Member[] memory) {
+        return memberList;
+    }
+    function getPartyList() public view returns (Party[] memory) {
+        return partyList;
+    }
+    function addMember(Member memory member) public{
+        memberList.push(member);
+        members[msg.sender] = member;
+        countMember++;
+    }
+    function addParty(Party memory party) public{
+        partyList.push(party);
+        parties[countParty] = party;
+        countParty++;
+    }
+}
